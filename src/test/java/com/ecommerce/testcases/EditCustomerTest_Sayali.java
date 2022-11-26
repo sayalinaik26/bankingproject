0e588cc3e4cadbe272a9d8eeae16c89a1e2ae6c5
sayalinaik26 <sayalikulkarni12@gmail.com>
2022-11-26 16:37:15 +0530

    DGTL:100 Changes to edit customer test.

--- src/test/java/com/ecommerce/testcases/EditCustomerTest_Sayali.java
+++ src/test/java/com/ecommerce/testcases/EditCustomerTest_Sayali.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -24,6 +26,7 @@ public class EditCustomerTest_Sayali extends BaseClass {
 	ManagerHomePom managerHomePom;
 	EditCustomerPom_Sayali editcustomer;
 	SoftAssert softAssert;
+	Sheet sh;
 		
 	
 	@BeforeClass
@@ -40,16 +43,28 @@ public class EditCustomerTest_Sayali extends BaseClass {
 	 * // public void testTitle() { // // // }
 	 */	
 	@Test(priority = 0)
-	public void testValidCustomerId() throws EncryptedDocumentException, IOException {
+	public void testValidCustomerId() throws EncryptedDocumentException, IOException, InterruptedException {
 		softAssert= new SoftAssert();
 		excelReader = new ExcelReader();
 		loginPagePom = new LoginPagePom();
-		Sheet sh = excelReader.getSheet("EditCustomer");
+		Sheet sh = excelReader.getSheet("loginpage");
 		Map<String, Object> data = excelReader.getData(sh);
 		//System.out.println(data);
 		loginPagePom.setWithValidCredentials((String) data.get("userid"), (String) data.get("password"));
 		loginPagePom.clickOnLoginButton();
+		managerHomePom = new ManagerHomePom();
 		managerHomePom.clickOnEditCustomer();
+		Thread.sleep(5000);
+
+		
+		Alert alert = driver.switchTo().alert();
+		//driver.findElement(By.xpath("//div[@class='ns-qe2n6-e-14 button-common close-button']")).click();
+		driver.findElement(By.xpath("//div[@id='dismiss-button']")).click();
+		Thread.sleep(2000);
+		//alert.dismiss();
+		excelReader.getSingleData(sh, 0, 0);
+		sh = excelReader.getSheet("EditCustomer");
+		System.out.println(sh);
 		editcustomer.setValidCustomerId((String)data.get("validCustomerId"));
 		softAssert.assertEquals("Characters are not allowed", "Characters are not allowed");
 		editcustomer.clickONSubmitButton();
